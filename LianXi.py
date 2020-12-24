610248b8263a5085052d6bb667745f21c0711c5e
c964309085 <31029583+c964309085@users.noreply.github.com>
2020-12-24 21:56:19 +0800

    heroku-app1

---
+++ LianXi.py
@@ -0,0 +1,9 @@
+import dash_core_components as dcc   
+dcc.Dropdown(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='MTL'
+)
